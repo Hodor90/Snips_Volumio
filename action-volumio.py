dc87a8d528a318dfcdfee83a3ac0a25a3bff8d25
Jochen <Jochen.Kehrle@gmail.com>
2019-05-06 14:37:16 +0200

    Update action for tests

--- action-volumio.py
+++ action-volumio.py
@@ -9,6 +9,7 @@ import io
 
 CONFIG_INI = "config.ini"
 
+
 class Volumio():
     """Class used to wrap action code with mqtt connection
         
@@ -19,21 +20,21 @@ class Volumio():
         # get the configuration if needed
         try:
             self.config = SnipsConfigParser.read_configuration_file(CONFIG_INI)
-        except :
+        except:
             self.config = None
 
-        self.base_api_url = "http://{0}:{1}/api/v1/commands/?cmd=".format(self.config['global']['host'], self.config['global']['port'])
+        self.base_api_url = "http://{0}:{1}/api/v1/commands/?cmd=".format(self.config['global']['host'],
+                                                                          self.config['global']['port'])
 
         # start listening to MQTT
         self.start_blocking()
-        
+
     # --> Sub callback function, one per intent
     def intent_play_callback(self, hermes, intent_message):
         # terminate the session first if not continue
         hermes.publish_end_session(intent_message.session_id, "Play")
-        
+
         # action code goes here...
-        print '[Received] intent: {}'.format(intent_message.intent.intent_name)
 
         # if need to speak the execution result by tts
         hermes.publish_start_session_notification(intent_message.site_id, "Action1 has been done", "")
@@ -43,7 +44,6 @@ class Volumio():
         hermes.publish_end_session(intent_message.session_id, "Stop")
 
         # action code goes here...
-        print '[Received] intent: {}'.format(intent_message.intent.intent_name)
 
         # if need to speak the execution result by tts
         hermes.publish_start_session_notification(intent_message.site_id, "Action2 has been done", "")
@@ -51,7 +51,7 @@ class Volumio():
     # More callback function goes here...
 
     # --> Master callback function, triggered everytime an intent is recognized
-    def master_intent_callback(self,hermes, intent_message):
+    def master_intent_callback(self, hermes, intent_message):
         coming_intent = intent_message.intent.intent_name
         if coming_intent == 'Play':
             self.intent_play_callback(hermes, intent_message)
@@ -64,9 +64,11 @@ class Volumio():
     def start_blocking(self):
         mqtt_addr = "{}:{}".format(self.config['global']['mqtt_ip_addr'], self.config['global']['mqtt_port'])
 
-        mqtt_opts = MqttOptions(username=self.config['secret']['mqtt_user'], password=self.config['secret']['mqtt_pw'], broker_address=mqtt_addr)
+        mqtt_opts = MqttOptions(username=self.config['secret']['mqtt_user'], password=self.config['secret']['mqtt_pw'],
+                                broker_address=mqtt_addr)
         with Hermes(mqtt_options=mqtt_opts) as h:
             h.subscribe_intents(self.master_intent_callback).start()
+
 
 if __name__ == "__main__":
     Volumio()
