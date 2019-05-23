ec9400a6a273473d92cbe03f9a4798eb47728ab7
Jochen <Jochen.Kehrle@gmail.com>
2019-05-23 22:33:20 +0200

    Fix setVolume

--- action-volumio.py
+++ action-volumio.py
@@ -30,6 +30,34 @@ class Volumio(object):
         # start listening to MQTT
         self.start_blocking()
 
+    def get_int_of_word(self, word):
+        wordList = ["null", "eins", "zwei", "drei", "view", "fünf", "sechs", "sieben", "acht", "neun", "zehn", "elf",
+                    "zwölf", "deizehn", "vierzehn", "fünfzehn", "sechzehn", "siebzehn", "achtzehn", "neunzehn",
+                    "zwanzig", "ein und zwanzig", "zwei und zwanzig", "drei und zwanzig", "vier und zwanzig",
+                    "fünf und zwanzig", "sechs und zwanzig", "sieben und zwanzig", "acht und zwanzig",
+                    "neun und zwanzig", "dreißig", "ein und dreißig", "zwei und dreißig", "drei und dreißig",
+                    "vier und dreißig", "fünf und dreißig", "sechs und dreißig", "sieben und dreißig",
+                    "acht und dreißig", "neun und dreißig", "vierzig", "ein und vierzig", "zwei und vierzig",
+                    "drei und vierzig", "vier und vierzig", "fünf und vierzig", "sechs und vierzig",
+                    "sieben und vierzig", "acht und vierzig", "neun und vierzig", "fünfzig", "ein und fünfzig",
+                    "zwei und fünfzig", "drei und fünfzig", "vier und fünfzig", "fünf und fünfzig", "sechs und fünfzig",
+                    "sieben und fünfzig", "acht und fünfzig", "neun und fünfzig", "sechzig", "ein und sechzig",
+                    "zwei und sechzig", "drei und sechzig", "vier und sechzig", "fünf und sechzig", "sechs und sechzig",
+                    "sieben und sechzig", "acht und sechzig", "neun und sechzig", "siebzig", "ein und siebzig",
+                    "zwei und siebzig", "drei und siebzig", "vier und siebzig", "fünf und siebzig", "sechs und siebzig",
+                    "sieben und siebzig", "acht und siebzig", "neun und siebzig", "achtzig", "ein und achtzig",
+                    "zwei und achtzig", "drei und achtzig", "vier und achtzig", "fünf und achtzig", "sechs und achtzig",
+                    "sieben und achtzig", "acht und achtzig", "neun und achtzig", "neunzig", "ein und neunzig",
+                    "zwei und neunzig", "drei und neunzig", "vier und neunzig", "fünf und neunzig", "sechs und neunzig",
+                    "sieben und neunzig", "acht und neunzig", "neun und neunzig", "einhundert"]
+
+        if word > 100:
+            return 100
+        elif word < 0:
+            return 0
+        else:
+            return wordList.index(word)
+
     # --> Sub callback function, one per intent
     def intent_play_callback(self, hermes, intent_message):
         # terminate the session first if not continue
@@ -77,7 +105,8 @@ class Volumio(object):
 
         if responsejson["response"] == "prev Success":
             # answer success
-            hermes.publish_start_session_notification(intent_message.site_id, "Ok, ich spiele die vorherige Wiedergabe.", "")
+            hermes.publish_start_session_notification(intent_message.site_id,
+                                                      "Ok, ich spiele die vorherige Wiedergabe.", "")
 
     def intent_next_callback(self, hermes, intent_message):
         # terminate the session first if not continue
@@ -89,23 +118,25 @@ class Volumio(object):
 
         if responsejson["response"] == "next Success":
             # answer success
-            hermes.publish_start_session_notification(intent_message.site_id, "Ok, ich spiele die nächste Wiedergabe.", "")
+            hermes.publish_start_session_notification(intent_message.site_id, "Ok, ich spiele die nächste Wiedergabe.",
+                                                      "")
 
     def intent_volume_callback(self, hermes, intent_message):
         # terminate the session first if not continue
         hermes.publish_end_session(intent_message.session_id, "")
 
-        volumeNumber = intent_message.slots.volume
+        volumeNumber = self.get_int_of_word(intent_message.slots.volume)
 
         # action code goes here...
         if isinstance(volumeNumber, int):
-            response = requests.get(self.base_api_url + "volume&volume={0}".format())
+            response = requests.get(self.base_api_url + "volume&volume={0}".format(volumeNumber))
             responsejson = response.json()
 
             if responsejson["response"] == "volume Success":
                 # answer success
-                hermes.publish_start_session_notification(intent_message.site_id, "Ok, Lautstärke ist jetzt bei {0}.".format(intent_message.slots.volume), "")
-
+                hermes.publish_start_session_notification(intent_message.site_id,
+                                                          "Ok, Lautstärke ist jetzt bei {0}.".format(
+                                                              intent_message.slots.volume), "")
 
     # --> Master callback function, triggered everytime an intent is recognized
     def master_intent_callback(self, hermes, intent_message):
