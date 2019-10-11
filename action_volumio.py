a56ad4f4828eec07096903c5d2da2389fd8d6d83
Jochen Kehrle <Jochen.Kehrle@googlemail.com>
2019-10-11 16:22:32 +0200

    Clean old code

--- action_volumio.py
+++ action_volumio.py
@@ -27,31 +27,6 @@ class Volumio(object):
         # start listening to MQTT
         self.start_blocking()
 
-    @staticmethod
-    def get_int_of_word(word):
-        wordlist = ["null", "eins", "zwei", "drei", "view", "fünf", "sechs", "sieben", "acht", "neun", "zehn", "elf",
-                    "zwölf", "deizehn", "vierzehn", "fünfzehn", "sechzehn", "siebzehn", "achtzehn", "neunzehn",
-                    "zwanzig", "ein und zwanzig", "zwei und zwanzig", "drei und zwanzig", "vier und zwanzig",
-                    "fünf und zwanzig", "sechs und zwanzig", "sieben und zwanzig", "acht und zwanzig",
-                    "neun und zwanzig", "dreißig", "ein und dreißig", "zwei und dreißig", "drei und dreißig",
-                    "vier und dreißig", "fünf und dreißig", "sechs und dreißig", "sieben und dreißig",
-                    "acht und dreißig", "neun und dreißig", "vierzig", "ein und vierzig", "zwei und vierzig",
-                    "drei und vierzig", "vier und vierzig", "fünf und vierzig", "sechs und vierzig",
-                    "sieben und vierzig", "acht und vierzig", "neun und vierzig", "fünfzig", "ein und fünfzig",
-                    "zwei und fünfzig", "drei und fünfzig", "vier und fünfzig", "fünf und fünfzig", "sechs und fünfzig",
-                    "sieben und fünfzig", "acht und fünfzig", "neun und fünfzig", "sechzig", "ein und sechzig",
-                    "zwei und sechzig", "drei und sechzig", "vier und sechzig", "fünf und sechzig", "sechs und sechzig",
-                    "sieben und sechzig", "acht und sechzig", "neun und sechzig", "siebzig", "ein und siebzig",
-                    "zwei und siebzig", "drei und siebzig", "vier und siebzig", "fünf und siebzig", "sechs und siebzig",
-                    "sieben und siebzig", "acht und siebzig", "neun und siebzig", "achtzig", "ein und achtzig",
-                    "zwei und achtzig", "drei und achtzig", "vier und achtzig", "fünf und achtzig", "sechs und achtzig",
-                    "sieben und achtzig", "acht und achtzig", "neun und achtzig", "neunzig", "ein und neunzig",
-                    "zwei und neunzig", "drei und neunzig", "vier und neunzig", "fünf und neunzig", "sechs und neunzig",
-                    "sieben und neunzig", "acht und neunzig", "neun und neunzig", "einhundert"]
-
-        if word in wordlist:
-            return wordlist.index(word)
-
     # --> Sub callback function, one per intent
     def intent_play_callback(self, hermes, intent_message):
         # terminate the session first if not continue
@@ -109,8 +84,6 @@ class Volumio(object):
     def intent_volume_callback(self, hermes, intent_message):
         # terminate the session first if not continue
         hermes.publish_end_session(intent_message.session_id, "")
-
-        #volumenumber = self.get_int_of_word(intent_message.slots.volume)
 
         # action code goes here...
         #if volumenumber is not None:
